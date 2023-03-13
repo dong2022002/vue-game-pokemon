475e9a515338ee3274e490984f55844a5d1f19b4
TranDieuDong <dong200220@gmail.com>
2023-03-13 12:07:40 +0700

    create all folder and init MainScreen, Interact Screen

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
   },
   extends: [
@@ -8,11 +9,15 @@ module.exports = {
     "eslint:recommended",
     "plugin:prettier/recommended",
   ],
+  plugins: ["prettier"],
   parserOptions: {
     parser: "@babel/eslint-parser",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+  globals: {
+    _: true,
   },
 };
---
+++ src/components/InteractScreen.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>InteractScreen Here</h1>
+</template>
